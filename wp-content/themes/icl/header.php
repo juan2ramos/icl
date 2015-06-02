11d710abeaa25a8b57f7f2d9999868ac0c30cd69
juan2ramos <juan2ramos@gmail.com>
2015-06-02 17:06:23 -0500

    cambios isa

--- wp-content/themes/icl/header.php
+++ wp-content/themes/icl/header.php
@@ -28,6 +28,7 @@
 
     <!-- Estilos -->
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/normalize.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/style.css">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
     <?php wp_head(); ?>
